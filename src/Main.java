09d28f30205eea544630d3a8405ac1d82f6f67b4
1XSTRY <kosogornov99@gmail.com>
2023-09-19 21:38:14 +0500

    Completed task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
     }
+    public static void task1 (){
+        int a=17;
+        int b=18;
+        int user = a + (int) (Math.random() * ((b - a) + 1));
+        if (user >= 18)  {
+            System.out.println("Если возраст человека равен " + user + " , то он достиг совершеннолетия");
+        }
+
+        else {
+            System.out.println("Если возраст человека равен " + user + " , то он не достиг совершеннолетия");
+        }
+    }
+
+
+
 }
